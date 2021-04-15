c2d81a6a1b623e00f3882e4d123e8bd6cca65560
Gino Zaccardelli <ginozacc@gmail.com>
2021-04-15 15:51:35 -0400

    backup of local files

---
+++ WorkingWithEFCore/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WorkingWithEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryingCategories();
+            // FilteredIncludes();
+        }
+
+        static void QueryingCategories()
+        {
+            using (var db = new Northwind())
+            {
+                var loggerFactory = db.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(new ConsoleLoggerProvider());
+
+                WriteLine("Categories and how many products they have:");
+
+                // a query to get all categories and their related products
+                IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+                }
+            }
+        }
+
+        static void FilteredIncludes()
+        {
+            using (var db = new Northwind())
+            {
+                Write("Enter a minimum for units in stock: ");
+                string unitsInStock = ReadLine();
+                int stock = int.Parse(unitsInStock);
+
+                IQueryable<Category> cats = db.Categories.Include(c => c.Products.Where(p => p.Stock >= stock));
+
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+
+                    foreach(Product p in c.Products)
+                    {
+                        WriteLine($"  {p.ProductName} has {p.Stock} units in stock.");
+                    }
+                }
+            }
+        }
+    }
+}
